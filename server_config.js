5a77ad031824a1bcf191bde69557f21bd4152acf
837885876 <837885876@users.noreply.github.com>
2026-07-13 11:48:02 +0800

    auto: update 2026-07-13 11:48:02

--- server_config.js
+++ server_config.js
@@ -1,4 +1,4 @@
 // Auto-pushed by start.py on each launcher run — DO NOT EDIT MANUALLY
-window.SV_BACKEND_URL = "https://7e512b6.r3.cpolar.top";
-window.SV_FRONTEND_URL = "https://5066236d.r3.cpolar.top";
-window.SV_BACKEND_GENERATED_AT = "2026-07-13 00:25:43";
+window.SV_BACKEND_URL = "https://637129ad.r3.cpolar.top";
+window.SV_FRONTEND_URL = "https://158a0775.r3.cpolar.top";
+window.SV_BACKEND_GENERATED_AT = "2026-07-13 11:48:01";
